05b3c702f3eb90bafccea559352fbede9a31691c
amir zare <amirjoun2@gmail.com>
2022-03-26 13:58:12 +0430

    clear var_dump from index.php

--- index.php
+++ index.php
@@ -3,16 +3,7 @@ include_once __DIR__ . '/vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-// putenv($_ENV);
-// if(getenv('APP_ENV') === 'development') {
-//     $dotenv->load(__DIR__);
-// }
-// $dotenv->required('OTHER_VAR');
-$project_name = getenv('NAME');
-var_dump($project_name);
 
-
-var_dump($_ENV['PROJECT_NAME']);die;
 require_once __DIR__.'/src/public/index.php';
 
 ?>
