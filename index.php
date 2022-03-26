2af211d5889d2bffa91fdfcc4ae77d6d7e0a5aec
amir zare <amirjoun2@gmail.com>
2022-03-26 11:53:15 +0430

    add index.php and git data

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include_once __DIR__ . '/vendor/autoload.php';
-$dotenv = new Dotenv\Dotenv(__DIR__);
-$dotenv->load();
+// $dotenv = new Dotenv\Dotenv(__DIR__);
+// $dotenv->load();
 var_dump(getenv('PROJECT_NAME'));
 var_dump($_ENV['PROJECT_NAME']);die;
 require_once __DIR__.'/src/public/index.php';
