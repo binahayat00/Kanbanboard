96416034312f3999a437280e1df7f719ad4bca77
amir zare <amirjoun2@gmail.com>
2022-03-27 13:52:16 +0430

    reform Application.php add conditions for some warnings

--- src/classes/KanbanBoard/Application.php
+++ src/classes/KanbanBoard/Application.php
@@ -1,9 +1,11 @@
 <?php
+
 namespace KanbanBoard;
 
 use Michelf\Markdown;
 
-class Application {
+class Application
+{
 
 	public function __construct($github, $repositories, $paused_labels = array())
 	{
@@ -15,29 +17,25 @@ class Application {
 	public function board()
 	{
 		$ms = array();
-		foreach ($this->repositories as $repository)
-		{
-			foreach ($this->github->milestones($repository) as $data)
-			{
+		foreach ($this->repositories as $repository) {
+			foreach ($this->github->milestones($repository) as $data) {
 				$ms[$data['title']] = $data;
 				$ms[$data['title']]['repository'] = $repository;
 			}
 		}
 		ksort($ms);
 		$milestones = [];
-		foreach ($ms as $name => $data)
-		{
+		foreach ($ms as $name => $data) {
 			$issues = $this->issues($data['repository'], $data['number']);
 			$percent = self::_percent($data['closed_issues'], $data['open_issues']);
-			if($percent)
-			{
+			if ($percent) {
 				$milestones[] = array(
 					'milestone' => $name,
-					'url' => $data['html_url'],
+					'url' => isset($data['html_url']) ? $data['html_url'] : null,
 					'progress' => $percent,
-					'queued' => $issues['queued'],
-					'active' => $issues['active'],
-					'completed' => $issues['completed']
+					'queued' => isset($issues['queued']) ? $issues['queued'] : null,
+					'active' => isset($issues['active']) ? $issues['active'] : null,
+					'completed' => isset($issues['completed']) ? $issues['completed'] : null,
 				);
 			}
 		}
@@ -46,33 +44,36 @@ class Application {
 
 	private function issues($repository, $milestone_id)
 	{
-		$indexes = $this->github->issues($repository, $milestone_id);
-		foreach ($indexes as $ii)
-		{
+		$getedIssues = $this->github->issues($repository, $milestone_id);
+
+		foreach ($getedIssues as $ii) {
 			if (isset($ii['pull_request']))
 				continue;
 			$issues[$ii['state'] === 'closed' ? 'completed' : (($ii['assignee']) ? 'active' : 'queued')][] = array(
 				'id' => $ii['id'], 'number' => $ii['number'],
 				'title'            	=> $ii['title'],
 				'body'             	=> Markdown::defaultTransform($ii['body']),
-     			'url' => $ii['html_url'],
-				'assignee'         	=> (is_array($ii) && array_key_exists('assignee', $ii) && !empty($ii['assignee'])) ? $ii['assignee']['avatar_url'].'?s=16' : NULL,
+				'url' => $ii['html_url'],
+				'assignee'         	=> (is_array($ii) && array_key_exists('assignee', $ii) && !empty($ii['assignee'])) ? $ii['assignee']['avatar_url'] . '?s=16' : NULL,
 				'paused'			=> self::labels_match($ii, $this->paused_labels),
 				'progress'			=> self::_percent(
-											substr_count(strtolower($ii['body']), '[x]'),
-											substr_count(strtolower($ii['body']), '[ ]')),
+					substr_count(strtolower($ii['body']), '[x]'),
+					substr_count(strtolower($ii['body']), '[ ]')
+				),
 				'closed'			=> $ii['closed_at']
 			);
 		}
-		if(isset($issues)){
+
+		if (isset($issues['active'])) {
 			usort($issues['active'], function ($a, $b) {
 				return count($a['paused']) - count($b['paused']) === 0 ? strcmp($a['title'], $b['title']) : count($a['paused']) - count($b['paused']);
 			});
 			return $issues;
-		}
+		} else if (isset($issues))
+			return $issues;
 		else
 			return null;
-		}
+	}
 
 	private static function _state($issue)
 	{
@@ -86,7 +87,7 @@ class Application {
 
 	private static function labels_match($issue, $needles)
 	{
-		if(Utilities::hasValue($issue, 'labels')) {
+		if (Utilities::hasValue($issue, 'labels')) {
 			foreach ($issue['labels'] as $label) {
 				if (in_array($label['name'], $needles)) {
 					return array($label['name']);
@@ -94,15 +95,13 @@ class Application {
 			}
 		}
 		return array();
-
 	}
 
 	private static function _percent($complete, $remaining)
 	{
 		$total = $complete + $remaining;
-		if($total > 0)
-		{
-			$percent = ($complete OR $remaining) ? round($complete / $total * 100) : 0;
+		if ($total > 0) {
+			$percent = ($complete or $remaining) ? round($complete / $total * 100) : 0;
 			return array(
 				'total' => $total,
 				'complete' => $complete,
