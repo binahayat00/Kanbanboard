5c87d24275dddd59ccae3d611b4d815d164f324d
amir zare <amirjoun2@gmail.com>
2022-03-31 09:30:39 +0430

    change name of public/index.php to public/loader.php

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ include_once __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-require_once __DIR__.'/src/public/index.php';
+require_once __DIR__.'/src/public/loader.php';
 
 ?>
