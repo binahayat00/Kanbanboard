068c1242b95331793d18ca69f15eedca25a6c8f7
amir zare <amirjoun2@gmail.com>
2022-04-04 11:12:34 +0430

    add Lib(global class) and routes

--- src/route/handler.php
+++ src/route/handler.php
@@ -3,12 +3,22 @@
 namespace App\Route;
 
 use App\Controllers\GithubController;
+use Lib\Response;
+use Lib\Router;
 
 class Handler {
 
     public function boot(){
-        $githubController = new GithubController();
-        $result = $githubController->getMilestones();
+        $result = null;
+        Router::get($_SERVER['REQUEST_URI'], function () {
+            $result = (new GithubController())->getMilestones();
+
+        });
+        // $result = null;
+        // $curdir = dirname($_SERVER['REQUEST_URI'])."/";
+        // $baseUri = str_replace($curdir, '/', dirname($_SERVER['REQUEST_URI']));
+        // var_dump($baseUri);die;
+        var_dump($result);die;
         return $result;
     }
 
