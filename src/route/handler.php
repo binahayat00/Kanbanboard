33b72746dbc95091bc9adeba802038d14f0393db
amir zare <amirjoun2@gmail.com>
2022-04-04 13:01:05 +0430

    reform handler

--- src/route/handler.php
+++ src/route/handler.php
@@ -7,19 +7,21 @@ use Lib\Response;
 use Lib\Router;
 
 class Handler {
-
-    public function boot(){
-        $result = null;
-        Router::get($_SERVER['REQUEST_URI'], function () {
-            $result = (new GithubController())->getMilestones();
-
-        });
-        // $result = null;
-        // $curdir = dirname($_SERVER['REQUEST_URI'])."/";
-        // $baseUri = str_replace($curdir, '/', dirname($_SERVER['REQUEST_URI']));
-        // var_dump($baseUri);die;
-        var_dump($result);die;
-        return $result;
+    protected $result;
+    public function __construct()
+    {
+        $this->result;
     }
+    public function boot(){
+
+        //every route
+        Router::get($_SERVER['REQUEST_URI'], function () {
+             $this->result = (new GithubController())->getMilestones();
+        });
+
+        return $this->result;
+    }
+
+
 
 }
