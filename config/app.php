22be4669762b7eff1b43b0515604e6689790039a
amir zare <amirjoun2@gmail.com>
2022-04-04 14:54:22 +0430

    add config.php

--- config/app.php
+++ config/app.php
@@ -1,8 +1,12 @@
+
 <?php
+// namespace Config;
 
-namespace Config;
-
-define('STATE','LKHYgbn776tgubkjhk'); 
-define('ACCESS_TOKEN_LINK','https://github.com/login/oauth/access_token'); 
-
+// define('STATE','LKHYgbn776tgubkjhk'); 
+// define('ACCESS_TOKEN_LINK','https://github.com/login/oauth/access_token'); 
+return [
+ 'LOG_PATH' => __DIR__ . './logs',
+ 'DB_PATH' => __DIR__ . '/db.json',
+ 'STATE' => 'LKHYgbn776tgubkjhk',
+];
 
