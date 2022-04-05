3c4b6d82c0e4a50f02057765649912cb8f0c71cb
amir zare <amirjoun2@gmail.com>
2022-04-05 12:43:19 +0430

    add cooment for callback

--- src/route/handler.php
+++ src/route/handler.php
@@ -19,7 +19,7 @@ class Handler {
              $this->result = (new GithubController())->getMilestones();
         });
 
-        //if wad not '/' route
+        //if wad not '/' route //callback from github
         $this->result = ($this->result) ? $this->result : (new GithubController())->getMilestones();
 
         return $this->result;
