268beef6bc8f1ea354b25a236ac9866b0a0717d5
amir zare <amirjoun2@gmail.com>
2022-04-11 08:35:56 +0430

    remove load .env

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 include_once __DIR__ . '/vendor/autoload.php';
 
-$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+// $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
+// $dotenv->load();
 
 use App\Route\Handler;
 
